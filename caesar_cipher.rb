2333923a4a14ed7e418f306cba44bf7b6fe2dc1f
holivane <holivane@gmail.com>
2024-06-09 22:36:09 -0300

    Fix caesar_cipher to pass in Rubocop

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,16 +1,21 @@
+# frozen_string_literal: true
+
 def caesar_cipher(str, shift)
-  code = ''
-  str.each_char do |char|
-    char_shift = if ('A'..'Z').include?(char)
-                   ((char.ord + shift - 'A'.ord) % 26 + 'A'.ord).chr
-                 elsif ('a'..'z').include?(char)
-                   ((char.ord + shift - 'a'.ord) % 26 + 'a'.ord).chr
-                 else
-                   char
-                 end
-    code << char_shift
-  end
+  code = str.chars.map do |char|
+    base = calculate_base(char)
+    base.is_a?(Integer) ? ((char.ord + shift - base) % 26 + base).chr : base
+  end.join
   puts code
 end
 
-caesar_cipher('What a string!', 26)
+def calculate_base(char)
+  if ('A'..'Z').include?(char)
+    'A'.ord
+  elsif ('a'..'z').include?(char)
+    'a'.ord
+  else
+    char
+  end
+end
+
+caesar_cipher('What a string!', 1)
