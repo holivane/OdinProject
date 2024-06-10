31018e53f5f539041773e56136efc0a8c560dc15
holivane <holivane@gmail.com>
2024-06-09 22:42:09 -0300

    Fix mastermind to pass in Rubocop

--- mastermind.rb
+++ mastermind.rb
@@ -1,27 +1,29 @@
-puts "Vamos jogar Mastermind?"
-puts "1 - Jogar"
-puts "0 - Sair"
+# frozen_string_literal: true
+
+puts 'Vamos jogar Mastermind?'
+puts '1 - Jogar'
+puts '0 - Sair'
 
 choice = gets.to_i
 
 loop do
-  break if choice == 1 || choice == 0
-  puts "Opção inválida!"
+  break if [1, 0].include?(choice)
+
+  puts 'Opção inválida!'
   choice = gets.to_i
 end
 
 if choice == 1
-  puts "Vamos jogar!"
-  puts "Escolha um nível de dificuldade:"
-  puts "1 - Fácil"
-  puts "2 - Médio"
-  puts "3 - Difícil"
+  puts 'Vamos jogar!'
+  puts 'Escolha um nível de dificuldade:'
+  puts '1 - Fácil'
+  puts '2 - Médio'
+  puts '3 - Difícil'
 
-  dificulty = gets.to_i
+  gets.to_i
 
-elsif choice == 0
-  puts "Até mais!"
+elsif choice.zero?
+  puts 'Até mais!'
 else
-  puts "Opção inválida!"
+  puts 'Opção inválida!'
 end
-
