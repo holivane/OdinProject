5c8b594e0a70df3a3bf2938566946eedc3118904
holivane <holivane@gmail.com>
2024-06-09 22:43:10 -0300

    Fix script to pass in Rubocop

--- script.rb
+++ script.rb
@@ -1,14 +1,14 @@
+# frozen_string_literal: true
+
 require 'pry-byebug'
 
 def isogram?(string)
   original_length = string.length
-  string_array = string.downcase.split("")
-
-  binding.pry
+  string_array = string.downcase.split('')
 
   unique_length = string_array.uniq.length
   original_length == unique_length
 end
 
-answere = isogram?("Odin")
+answere = isogram?('Odin')
 puts answere
