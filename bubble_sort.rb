e30168061598a0558bf2a7ffa6a92eff57e47a32
holivane <holivane@gmail.com>
2024-06-09 22:41:15 -0300

    Fix bubble_sort to pass in Rubocop

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,18 +1,14 @@
+# frozen_string_literal: true
+
 def bubble_sort(array)
   n = array.length
   loop do
     swapped = false
-
     (n - 1).times do |i|
-      if array[i] > array[i + 1]
-        array[i], array[i + 1] = array[i + 1], array[i]
-        swapped = true
-      end
+      array[i], array[i + 1], swapped = array[i + 1], array[i], true if array[i] > array[i + 1]
     end
-
     break unless swapped
   end
-
   array
 end
 
