56d7094fff82cec7f1d373e6e7667cfc54c3add5
holivane <holivane@gmail.com>
2024-06-24 09:40:16 -0300

    Fix caesar_cipher

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # frozen_string_literal: true
 
 def caesar_cipher(str, shift)
@@ -20,21 +19,3 @@ def calculate_base(char)
 end
 
 caesar_cipher('What a string!', 1)
-=======
-def caesar_cipher(str, shift)
-    code = ""
-    str.each_char do |char|
-      if ('A'..'Z').include?(char)
-        char_shift = ((char.ord + shift - 'A'.ord) % 26 + 'A'.ord).chr
-      elsif ('a'..'z').include?(char)
-        char_shift = ((char.ord + shift - 'a'.ord) % 26 + 'a'.ord).chr
-      else
-        char_shift = char
-      end
-      code << char_shift
-    end
-    puts code
-  end
-  
-  caesar_cipher("What a string!", 5)
->>>>>>> 33249f1 (Método para modificar string)
