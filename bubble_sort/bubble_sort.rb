a3a6d3d31469946ec3bae5ce15bf4681b076710a
holivane <holivane@gmail.com>
2024-06-24 10:11:38 -0300

    Fix buble_sort

--- bubble_sort/bubble_sort.rb
+++ bubble_sort/bubble_sort.rb
@@ -1,19 +1,15 @@
+# frozen_string_literal: true
+
 def bubble_sort(array)
-    n = array.length
-    loop do
-      swapped = false
-  
-      (n - 1).times do |i|
-        if array[i] > array[i + 1]
-          array[i], array[i + 1] = array[i + 1], array[i]
-          swapped = true
-        end
-      end
-  
-      break unless swapped
+  n = array.length
+  loop do
+    swapped = false
+    (n - 1).times do |i|
+      array[i], array[i + 1], swapped = array[i + 1], array[i], true if array[i] > array[i + 1]
     end
-  
-    array
+    break unless swapped
   end
-  
-  print bubble_sort([4, 3, 78, 2, 0, 2])
+  array
+end
+
+print bubble_sort([4, 3, 78, 2, 0, 2])
