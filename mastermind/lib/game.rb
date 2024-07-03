74cb84777403502f018735b34023b639c55f05ac
holivane <holivane@gmail.com>
2024-07-03 16:41:51 -0300

    Apply class User and UserInterface in the Game class

--- mastermind/lib/game.rb
+++ mastermind/lib/game.rb
@@ -1,20 +1,24 @@
 # frozen_string_literal: true
+require_relative 'user'
+require_relative 'user_interface'
 
 # Classe responsável por controlar o fluxo do jogo
 # e interagir com o usuário
 class Game
   def initialize
-    puts 'Bem-vindo ao Mastermind!'
+    @ui = UserInterface.new
+    @user = User.new(@ui.prompt_user_for_name)
   end
 
   def play_game
+    @ui.display_instructions
     menu_options = {
       1 => method(:start_game),
       2 => method(:exit_game)
     }
 
-    puts 'Escolha uma opção:'
-    choice = prompt_user_for_choice('1 - Jogar | 2 - Sair: ', menu_options.keys)
+    puts "#{@user.name}, escolha uma opção:"
+    choice = UserInterface.prompt_user_for_choice('1 - Jogar | 2 - Sair: ', menu_options.keys)
     menu_options[choice].call
   end
 
@@ -24,18 +28,19 @@ class Game
     difficulty_options = {
       1 => ['Fácil', method(:easy)],
       2 => ['Médio', method(:medium)],
-      3 => ['Difícil', method(:hard)]
+      3 => ['Difícil', method(:hard)],
+      4 => ['<-- Voltar', method(:play_game)]
     }
 
     puts 'Escolha um nível de dificuldade:'
     difficulty_options.each { |key, (level, _)| puts "#{key} - #{level}" }
 
-    choice = prompt_user_for_choice('Escolha uma dificuldade: ', difficulty_options.keys)
+    choice = UserInterface.prompt_user_for_choice('Escolha uma dificuldade: ', difficulty_options.keys)
     difficulty_options[choice][1].call
   end
 
   def exit_game
-    puts 'Até mais!'
+    @ui.dispaly_exit_message(@user.name)
   end
 
   def easy
